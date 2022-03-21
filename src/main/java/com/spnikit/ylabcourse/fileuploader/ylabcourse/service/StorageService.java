e1af0bf265506790fae886c2be05f9de9689ccfb
spnikit <spnikit@gmail.com>
2022-03-21 20:45:38 +0300

    game plays in different thread

--- src/main/java/com/spnikit/ylabcourse/fileuploader/ylabcourse/service/StorageService.java
+++ src/main/java/com/spnikit/ylabcourse/fileuploader/ylabcourse/service/StorageService.java
@@ -3,7 +3,6 @@ package com.spnikit.ylabcourse.fileuploader.ylabcourse.service;
 
 import com.spnikit.ylabcourse.fileuploader.ylabcourse.game.Game;
 import com.spnikit.ylabcourse.fileuploader.ylabcourse.game.Gameplay;
-import com.spnikit.ylabcourse.fileuploader.ylabcourse.game.GameplayToFileMapper;
 import com.spnikit.ylabcourse.fileuploader.ylabcourse.game.GameplayToJsonMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,13 +10,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 
 
 @Slf4j
@@ -25,8 +19,8 @@ import java.util.Optional;
 public class StorageService {
 
 
-    public void load(MultipartFile file){
-        if(file.isEmpty()){
+    public void load(MultipartFile file) {
+        if (file.isEmpty()) {
             throw new IllegalArgumentException("file is empty");
         }
 
@@ -34,16 +28,22 @@ public class StorageService {
     }
 
 
+    private void playGameFromFile(MultipartFile file) {
 
-    private void playGameFromFile(MultipartFile file){
+        final var pool = Executors.newFixedThreadPool(1);
 
-        try(InputStream inputStream = file.getInputStream()) {
+        try (InputStream inputStream = file.getInputStream()) {
 
             Optional<Gameplay> gameplay = new GameplayToJsonMapper().readFromStream(inputStream);
 
-            gameplay.ifPresentOrElse(game -> new Game().replay(game),
-                    () -> log.error("Gameplay object is not valid or not present"));
+            pool.submit(() -> {
 
+                gameplay.ifPresentOrElse(game -> new Game().replay(game),
+                        () -> log.error("Gameplay object is not valid or not present"));
+
+            });
+
+            pool.shutdown();
         } catch (IOException e) {
             log.error("Can't read stream from file");
             e.printStackTrace();
