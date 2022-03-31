7503cb179e389969e15f37d41722a4281fecd898
spnikit <spnikit@gmail.com>
2022-04-01 00:20:49 +0300

    add input focus

--- src/frontend-tic-tac-toe/src/PlayerInput.js
+++ src/frontend-tic-tac-toe/src/PlayerInput.js
@@ -1,12 +1,13 @@
 import {Input} from '@chakra-ui/react'
 import {Button} from '@chakra-ui/react'
 import {Box} from '@chakra-ui/react'
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 
 function PlayerInput({setPlayerName, number}) {
 
     const [name, setName] = useState("");
+    const inputRef = useRef();
 
     const handleChange = (event) => {
         setName(event.target.value);
@@ -16,11 +17,12 @@ function PlayerInput({setPlayerName, number}) {
     const handleBtnClick = () => {
         setPlayerName(name)
         setName("");
+        inputRef.current.focus();
     }
 
     return (
         <Box display="flex" mb="2rem">
-            <Input value={name} onChange={handleChange} placeholder={`Enter Player ${number} name`}/>
+            <Input ref={inputRef} value={name} onChange={handleChange} placeholder={`Enter Player ${number} name`}/>
             <Button onClick={handleBtnClick} display="inline-block" colorScheme='blue' ml="1rem">OK</Button>
         </Box>
     );
